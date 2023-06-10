6d1a816c2c8620e7c856f949a600ed181bc5b33f
YuliyaAtrashkevich <trafimava@gmail.com>
2023-06-10 23:18:09 +0600

    Анимация галереи

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
   var sidenav = document.querySelectorAll('.sidenav');
   var instances_sidenav = M.Sidenav.init(sidenav);
+
   var modal = document.querySelectorAll('.modal');
   var instances_modal = M.Modal.init(modal);
+  
+  var materialbox = document.querySelectorAll('.materialboxed');
+  var instances_materialbox = M.Materialbox.init(materialbox);
 });
